1d153d5e5b89cd78a7823588b92423d8ab68bdbf
AF Tamim <itamim345@gmail.com>
2023-01-12 17:25:08 +0530

    Done with navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav>
-      <NavLink to="/">CRYPTO</NavLink>
+    <nav className='w-1/2 border-2 border-yellow-400 rounded mt-8 p-1 flex justify-between gap-2 text-center'>
+      <NavLink to="/" end className={({isActive})=>{
+        return `rounded my-1 w-1/3 ${
+          isActive ? "bg-yellow-400 text-black" : "bg-gray-600 hover:text-yellow-400"
+        }`;
+      }}>CRYPTO</NavLink>
+      <NavLink to="/trending" className={({isActive})=>{
+        return ` rounded my-1 w-1/3 ${
+          isActive ? "bg-yellow-400 text-black" : "bg-gray-600 hover:text-yellow-400"
+        }`;
+      }}>TRENDING</NavLink>
+      <NavLink to="/favourites" className={({isActive})=>{
+        return `bg-gray-600 rounded my-1 w-1/3 ${
+          isActive ? " bg-yellow-400 text-black" : "bg-gray-600 hover:text-yellow-400"
+        }`;
+      }}>FAVOURITES</NavLink>
     </nav>
   );
 }
