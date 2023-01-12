49bba04360408be9049bf05fbf1d280634424cb8
AF Tamim <itamim345@gmail.com>
2023-01-12 16:05:23 +0530

    Created navbar component and added into home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Logo from '../components/Logo';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div className="h-full w-full flex flex-col items-center content-center text-white relative">
       <div className="bg-gray-400 w-screen h-screen fixed -z-20"></div>
       <Logo />
+      <Navbar/>
       <Outlet />
     </div>
   );
