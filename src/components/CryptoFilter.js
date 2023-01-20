9e9ef59fe5a80be072b71c7ccbaff839b766f94e
AF Tamim <itamim345@gmail.com>
2023-01-20 23:41:01 +0530

    Added a function to change the currency value

--- src/components/CryptoFilter.js
+++ src/components/CryptoFilter.js
@@ -5,11 +5,19 @@ import CryptoSearch from './CryptoSearch';
 export default function CryptoFilter() {
   const setCurrency = useContext(CryptoContext);
   const currencyRef = useRef(null);
+
+  const handleCurrencySubmit = (e) => {
+    e.preventDefault();
+    let currencyInput = currencyRef.current.value;
+    setCurrency(currencyInput);
+  }
+
+
   return (
     <div className="w-full border-2 mt-6 rounded border-zinc-300 flex justify-between p-2">
       <CryptoSearch />
       <div>
-        <form className="flex gap-1 items-center w-25">
+        <form className="flex gap-1 items-center w-25" onSubmit={handleCurrencySubmit}>
           <label htmlFor="currency">Currency:</label>
           <input
             type="text"
@@ -17,6 +25,7 @@ export default function CryptoFilter() {
             id="currency"
             className="w-1/4 bg-zinc-600 rounded p-1 border border-transparent focus:border-yellow-300 outline-none"
             placeholder="usd"
+            ref={currencyRef}
           />
           <button type="submit" className='flex'>
             <i className="ri-exchange-box-line text-yellow-400 text-4xl"></i>
