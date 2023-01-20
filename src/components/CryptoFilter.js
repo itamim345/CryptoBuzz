ee75ce636dd2852c2c569437d8c1311813f8f316
AF Tamim <itamim345@gmail.com>
2023-01-21 00:26:01 +0530

    Added Sort By section

--- src/components/CryptoFilter.js
+++ src/components/CryptoFilter.js
@@ -17,8 +17,11 @@ export default function CryptoFilter() {
   return (
     <div className="w-full border-2 mt-6 rounded border-zinc-300 flex justify-between p-2">
       <CryptoSearch />
-      <div>
-        <form className="flex gap-1 items-center w-25" onSubmit={handleCurrencySubmit}>
+      <div className='w-1/3'>
+        <form
+          className="flex gap-1 items-center justify-center w-25"
+          onSubmit={handleCurrencySubmit}
+        >
           <label htmlFor="currency">Currency:</label>
           <input
             type="text"
@@ -28,12 +31,31 @@ export default function CryptoFilter() {
             placeholder="usd"
             ref={currencyRef}
           />
-          <button type="submit" className='flex'>
+          <button type="submit" className="flex">
             <i className="ri-exchange-box-line text-yellow-400 text-4xl"></i>
           </button>
         </form>
       </div>
-      <div>Filter</div>
+      {/*************  filter *************/}
+      <div className="flex items-center w-1/3 justify-end">
+        <label>
+          <span>Sort by : </span>
+          <select
+            name="sortby"
+            className='bg-zinc-600 rounded outline-0 p-1 border border-transparent focus:border-yellow-300 outline-none"
+          placeholder="search..'
+          >
+            <option value="market_cap_desc">market cap desc</option>
+            <option value="market_cap_asc">market cap asc</option>
+            <option value="gecko_desc">gecko desc</option>
+            <option value="gecko_asc">gecko asc</option>
+            <option value="market_cap_asc">market cap asc</option>
+            <option value="volume_desc">volume desc</option>
+            <option value="id_asc">id asc</option>
+            <option value="id_desc">id desc</option>
+          </select>
+        </label>
+      </div>
     </div>
   );
 }
