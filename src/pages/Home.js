1cc188212ff740adcea161da020de55a03ce6bca
AF Tamim <itamim345@gmail.com>
2023-01-23 07:38:43 +0530

    Added trendingProvider into HOme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Logo from '../components/Logo';
-import Navbar from '../components/Navbar';
-import { CryptoProvider } from '../context/CryptoContext';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
+import Navbar from "../components/Navbar";
+import { CryptoProvider } from "../context/CryptoContext";
+import { TrendingProvider } from "../context/TrendingContext";
 
 export default function Home() {
   return (
     <CryptoProvider>
-      <div className="h-full w-full flex flex-col items-center content-center text-white relative pb-8">
-        <div className="bg-zinc-900 w-screen h-screen fixed -z-20"></div>
-        <Logo />
-        <Navbar />
-        <Outlet />
-      </div>
+      <TrendingProvider>
+        <div className="h-full w-full flex flex-col items-center content-center text-white relative pb-8">
+          <div className="bg-zinc-900 w-screen h-screen fixed -z-20"></div>
+          <Logo />
+          <Navbar />
+          <Outlet />
+        </div>
+      </TrendingProvider>
     </CryptoProvider>
   );
 }
